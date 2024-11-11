58f1f46f4a322ea6dfb1a4796db9e389a2c09b8b
danielmerbet <damebet@gmail.com>
2024-11-11 22:05:01 +0100

    dynamic port

--- app.py
+++ app.py
@@ -162,5 +162,5 @@ def shutdown_scheduler(exception=None):
         pass
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    #  app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    #app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
